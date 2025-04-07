fd213756adca18a28409ba27ca953d1c6e49d8c2
mihi <mitopalovic@edu.aau.at>
2025-04-07 13:49:55 +0200

    Test hinzugefügt für einen TestClient

--- src/main/java/TestClient.java
+++ src/main/java/TestClient.java
@@ -1,39 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 
 public class TestClient {
     public static void main(String[] args) {
-        try{
-            Socket socket = new Socket("localhost", 8081);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+        new TestClient().runClient(System.in, System.out);
+    }
 
-
-            new Thread(() -> {
-                try{
-                    String server;
-                    while((server = in.readLine()) != null) {
-                        System.out.println(server);
+    public void runClient(InputStream inStream, PrintStream outStream) {
+        try (
+                Socket socket = new Socket("localhost", 8081);
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                BufferedReader stdIn = new BufferedReader(new InputStreamReader(inStream));
+        ) {
+            Thread listener = new Thread(() -> {
+                try {
+                    String serverMessage;
+                    while ((serverMessage = in.readLine()) != null) {
+                        outStream.println(serverMessage);
                     }
                 } catch (IOException e) {
-                    System.out.println("Verbindung zum Server verloren.");
+                    outStream.println("Verbindung zum Server verloren.");
                 }
-            }).start();
+            });
+
+            listener.setDaemon(true);
+            listener.start();
 
             String userMessage;
-            while((userMessage = stdIn.readLine()) != null) {
+            while ((userMessage = stdIn.readLine()) != null) {
                 out.println(userMessage);
-                if(userMessage.equals("bye")) {
+                if ("bye".equalsIgnoreCase(userMessage)) {
                     break;
                 }
             }
 
-        } catch(Exception e) {
-            System.out.println("Fehler beim Verbinden");
+        } catch (IOException e) {
+            outStream.println("Fehler beim Verbinden:");
+            e.printStackTrace(outStream);
         }
     }
 }
