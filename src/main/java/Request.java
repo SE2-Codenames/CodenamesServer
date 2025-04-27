5e872e7455fe0eae7e2fc1e73998fad4469b9a01
mihi <mitopalovic@edu.aau.at>
2025-04-27 19:36:56 +0200

    payload interfaces + classes response/request for server

---
+++ src/main/java/Request.java
@@ -0,0 +1,26 @@
+public class Request {
+    private String type;
+    private Payload payload;
+
+    public Request(String type, Payload payload) {
+        this.type = type;
+        this.payload = payload;
+    }
+
+    //Getter und Setter
+    public String getType() {
+        return type;
+    }
+
+    public Payload getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Payload payload) {
+        this.payload = payload;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
