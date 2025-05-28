7bebc8f37dfde7ecec8c526aac21cba57e2a0fb2
Stefan Ckonjovic <stefanck@edu.aau.at>
2025-05-28 21:11:07 +0200

    added test to check if username is taken

--- src/test/java/ConnectionTest.java
+++ src/test/java/ConnectionTest.java
@@ -38,4 +38,49 @@ public class ConnectionTest {
 
         assertTrue(connected, "Server should accept connections on 0.0.0.0");
     }
+
+    @Test
+    public void testUsernameTaken() throws Exception {
+        int port = 9091;
+        ServerImpl server = new ServerImpl(port);
+        server.start();
+
+        URI uri = new URI("ws://localhost:" + port);
+        CountDownLatch takenLatch = new CountDownLatch(1);
+
+        WebSocketClient client1 = new WebSocketClient(uri) {
+            @Override public void onOpen(ServerHandshake handshakedata) {
+                send("USER:Stefan");
+            }
+            @Override public void onMessage(String message) {}
+            @Override public void onClose(int code, String reason, boolean remote) {}
+            @Override public void onError(Exception ex) {}
+        };
+
+        WebSocketClient client2 = new WebSocketClient(uri) {
+            @Override public void onOpen(ServerHandshake handshakedata) {
+                send("USER:Stefan");  //in case that name is different, test will fail
+            }
+            @Override public void onMessage(String message) {
+                if (message.equals("USERNAME_TAKEN")) {
+                    takenLatch.countDown();
+                }
+            }
+            @Override public void onClose(int code, String reason, boolean remote) {}
+            @Override public void onError(Exception ex) {}
+        };
+
+        client1.connectBlocking();
+        Thread.sleep(200);
+        client2.connect();
+
+        boolean takenMessageReceived = takenLatch.await(3, TimeUnit.SECONDS);
+
+        client1.close();
+        client2.close();
+        server.stop();
+
+        assertTrue(takenMessageReceived, "Server should respond with USERNAME_TAKEN for duplicate usernames.");
+    }
+
 }
