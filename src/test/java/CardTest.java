0302447d0a7e2aa8d017b3390ecbde9e038fa925
Pablo Pirker Garcia <pablopi@edu.aau.at>
2025-06-14 10:11:44 +0200

    Removed public modifier for CardTest and GameProgressTest as demanded by Sonar for maintainability

--- src/test/java/CardTest.java
+++ src/test/java/CardTest.java
@@ -5,60 +5,60 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class CardTest {
+class CardTest {
     private Card redTeamCard;
     private Card blueTeamCard;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         redTeamCard = new Card("Dog", CardRole.RED);
         blueTeamCard = new Card("Cat", CardRole.BLUE);
     }
 
     @Test
-    public void constructorTestRed() {
+    void constructorTestRed() {
         assertEquals("Dog", redTeamCard.getWord());
         assertEquals(CardRole.RED, redTeamCard.getCardRole());
         assertFalse(redTeamCard.isRevealed());
     }
 
     @Test
-    public void constructorTestBlue() {
+    void constructorTestBlue() {
         assertEquals("Cat", blueTeamCard.getWord());
         assertEquals(CardRole.BLUE, blueTeamCard.getCardRole());
         assertFalse(blueTeamCard.isRevealed());
     }
 
     @Test
-    public void isRevealedTestRed() {
+    void isRevealedTestRed() {
         redTeamCard.reveal();
         assertTrue(redTeamCard.isRevealed());
     }
 
     @Test
-    public void isRevealedTestBlue() {
+    void isRevealedTestBlue() {
         blueTeamCard.reveal();
         assertTrue(blueTeamCard.isRevealed());
     }
 
     @Test
-    public void toStringTestRed() {
+    void toStringTestRed() {
         assertEquals("Dog [RED]", redTeamCard.toString());
     }
 
     @Test
-    public void toStringTestBlue() {
+    void toStringTestBlue() {
         assertEquals("Cat [BLUE]", blueTeamCard.toString());
     }
 
     @Test
-    public void toStringTest2Red() {
+    void toStringTest2Red() {
         redTeamCard.reveal();
         assertEquals("Dog [RED] (revealed)", redTeamCard.toString());
     }
 
     @Test
-    public void toStringTest2Blue() {
+    void toStringTest2Blue() {
         blueTeamCard.reveal();
         assertEquals("Cat [BLUE] (revealed)", blueTeamCard.toString());
     }
